691d58d0190890ff151e822d2e8f9dbe4c5812e7
diifferent <qc_eedd95446de6491eac70435c19806c55@git.code.tencent.com>
2018-12-12 15:52:12 +0800

    增加求和

---
+++ src/求和.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int f(int n){
+    if(n == 1){
+        return 1;
+    }
+    else{
+        return f(n-1) + n;
+    }
+}
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    printf("%d",f(n));
+}
